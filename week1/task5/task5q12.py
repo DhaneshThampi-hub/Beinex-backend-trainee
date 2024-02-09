e631556374d5ca78292dd6b4a2cf53273db015b6
DhaneshThampi-hub <159514392+DhaneshThampi-hub@users.noreply.github.com>
2024-02-09 13:44:23 +0530

    Update task5q12.py

--- week1/task5/task5q12.py
+++ week1/task5/task5q12.py
@@ -1,6 +1,6 @@
 # Using for loop 
 fibonacci_sequence = [0, 1]
-for _ in range(8):
+for i in range(8):
     next_number = fibonacci_sequence[-1] + fibonacci_sequence[-2]
     fibonacci_sequence.append(next_number)
 print("First 10 Fibonacci numbers using 'for' loop:", fibonacci_sequence)
