4beb9d12b67dc69e3033be03d266ea9927b8e689
DhaneshThampi-hub <159514392+DhaneshThampi-hub@users.noreply.github.com>
2024-02-09 14:00:59 +0530

    Delete week1 directory

--- week1/task5/task5q10.py
+++
@@ -1,7 +0,0 @@
-start = int(input("Enter the starting number: "))
-end = int(input("Enter the ending number: "))
-even_sum = 0
-for number in range(start, end + 1):
-    if number % 2 == 0:
-        even_sum += number
-print(f"The sum of even numbers between {start} and {end} is: {even_sum}")
